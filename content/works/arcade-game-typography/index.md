5ab9ef138f602a1b01c1893415b62b9819b28e29
Xiangdong Zeng <xdzeng96@gmail.com>
2021-08-12 22:25:52 +0800

    mobile version

---
+++ content/works/arcade-game-typography/index.md
@@ -0,0 +1,11 @@
+---
+title: Arcade Game Typography
+extra:
+  type: Translation
+  designer: Telos
+  publisher: Telos
+  translator: Willie Liu
+  editor: Xiyao Du
+  images:
+    - 资源 1.jpg
+---
--- static/script.js
+++ static/script.js
@@ -1,10 +1,10 @@
 (function () {
-    updateHeight();
+    updateMainHeight();
 })();
 
-function updateHeight() {
+function updateMainHeight() {
     const footerHeight = document.querySelector('footer').clientHeight + 'px'
-    const margin = window.getComputedStyle(document.querySelector('body')).margin;
+    const margin = window.getComputedStyle(document.querySelector('body')).marginBottom;
     document.querySelector('main').style.minHeight =
         `calc(100vh - ${footerHeight} - ${margin} * 2)`;
 }
