3f05b2eca537b19c932ed8b01de0f52fc8ba751f
Xiangdong Zeng <xdzeng96@gmail.com>
2021-08-14 04:09:06 +0800

    nav menu toggle

--- static/script.js
+++ static/script.js
@@ -1,6 +1,24 @@
 (function () {
-    updateMainHeight();
+    updateNavButtons();
+    // updateMainHeight();
 })();
+
+function updateNavButtons() {
+    const showButton = document.querySelector('#show-sidebar');
+    const closeButton = document.querySelector('#close-sidebar');
+    const sidebar = document.querySelector('#sidebar-headings');
+
+    showButton.onclick = () => {
+        sidebar.classList.add('show');
+        showButton.classList.remove('show');
+        closeButton.classList.add('show');
+    }
+    closeButton.onclick = () => {
+        sidebar.classList.remove('show');
+        showButton.classList.add('show');
+        closeButton.classList.remove('show');
+    }
+}
 
 function updateMainHeight() {
     const footerHeight = document.querySelector('footer').clientHeight + 'px'
