a34ddca81cc14c59a34dd24441f359e7c00fe4ca
Xiangdong Zeng <xdzeng96@gmail.com>
2021-08-14 22:09:14 +0800

    carousel [WIP]

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
 (function () {
     updateFooterHeight();
     updateNavButtons();
+    updateCarouselButtons();
 })();
 
 function updateFooterHeight() {
@@ -19,6 +20,38 @@ function updateNavButtons() {
         } else {
             sidebar.classList.add("show");
             toggleButton.classList.add("close");
+        }
+    }
+}
+
+function updateCarouselButtons() {
+    const carouselItems = document.querySelectorAll(".carousel-inner .carousel-item");
+    const carouselIndicators = document.querySelectorAll(".carousel-indicators button");
+    const carouselNum = carouselItems.length;
+
+    const activate = (index) => {
+        carouselItems[index].classList.add("active");
+        carouselIndicators[index].classList.add("active");
+    }
+    const inactivate = (index) => {
+        carouselItems[index].classList.remove("active");
+        carouselIndicators[index].classList.remove("active");
+    }
+
+    if (carouselNum > 0) {
+        // let carouselIndex = 0;
+        let carouselIndex = Math.floor(Math.random() * carouselNum);
+        activate(carouselIndex);
+
+        document.querySelector(".carousel-control-prev").onclick = () => {
+            inactivate(carouselIndex);
+            carouselIndex = carouselIndex === 0 ? carouselNum - 1 : carouselIndex - 1;
+            activate(carouselIndex);
+        }
+        document.querySelector(".carousel-control-next").onclick = () => {
+            inactivate(carouselIndex);
+            carouselIndex = carouselIndex === carouselNum - 1 ? 0 : carouselIndex + 1;
+            activate(carouselIndex);
         }
     }
 }
