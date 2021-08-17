dc6e1d3147b2f61eddc377fbf6127c22e03532aa
Xiangdong Zeng <xdzeng96@gmail.com>
2021-08-18 02:12:57 +0800

    homepage, cursor

--- static/script.js
+++ static/script.js
@@ -1,10 +1,10 @@
 (function () {
-    updateFooterHeight();
+    calcFooterHeight();
     updateNavButtons();
-    updateCarouselButtons();
+    updateCarousel();
 })();
 
-function updateFooterHeight() {
+function calcFooterHeight() {
     const footerHeight = document.querySelector("footer").clientHeight + "px";
     document.documentElement.style.setProperty("--footer-height", footerHeight);
 }
@@ -24,34 +24,28 @@ function updateNavButtons() {
     }
 }
 
-function updateCarouselButtons() {
+function updateCarousel() {
     const carouselItems = document.querySelectorAll(".carousel-inner .carousel-item");
-    const carouselIndicators = document.querySelectorAll(".carousel-indicators button");
     const carouselNum = carouselItems.length;
 
-    const activate = (index) => {
-        carouselItems[index].classList.add("active");
-        carouselIndicators[index].classList.add("active");
+    const activate = (i) => {
+        const elem = carouselItems[i];
+        elem.classList.add("show");
+        setTimeout(() => { elem.classList.add("visible") }, 0);
     }
-    const inactivate = (index) => {
-        carouselItems[index].classList.remove("active");
-        carouselIndicators[index].classList.remove("active");
+    const inactivate = (i) => {
+        const elem = carouselItems[i];
+        elem.classList.remove("visible");
+        elem.classList.remove("show");
     }
 
-    if (carouselNum > 0) {
-        // let carouselIndex = 0;
-        let carouselIndex = Math.floor(Math.random() * carouselNum);
+    let carouselIndex = Math.floor(Math.random() * carouselNum);
+    activate(carouselIndex);
+
+    const showNext = () => {
+        inactivate(carouselIndex);
+        carouselIndex = carouselIndex === carouselNum - 1 ? 0 : carouselIndex + 1;
         activate(carouselIndex);
-
-        document.querySelector(".carousel-control-prev").onclick = () => {
-            inactivate(carouselIndex);
-            carouselIndex = carouselIndex === 0 ? carouselNum - 1 : carouselIndex - 1;
-            activate(carouselIndex);
-        }
-        document.querySelector(".carousel-control-next").onclick = () => {
-            inactivate(carouselIndex);
-            carouselIndex = carouselIndex === carouselNum - 1 ? 0 : carouselIndex + 1;
-            activate(carouselIndex);
-        }
     }
+    setInterval(showNext, 8000);
 }
