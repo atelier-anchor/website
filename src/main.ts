3d4e2146fd626481aa44969943ab84389f23a7e9
Xiangdong Zeng <xdzeng96@gmail.com>
2022-05-22 21:36:18 +0800

    use vite-ssg

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
-import { createApp } from 'vue'
+import { ViteSSG } from 'vite-ssg'
 
 import App from '@/App.vue'
-import router from '@/router'
+import routes from '@/routes'
 
 import 'virtual:windi.css'
 import '@/styles/index.css'
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+export const createApp = ViteSSG(App, { routes })
