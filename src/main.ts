908cb4b399bac61075aee19f9c38742cd9315e04
Xiangdong Zeng <xdzeng96@gmail.com>
2022-06-04 18:44:31 +0800

    `src/styles/index.css` -> `src/index.css`

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { ViteSSG } from 'vite-ssg'
-
 import App from '@/App.vue'
 import routes from '@/routes'
-
-import '@/styles/index.css'
+import '@/index.css'
 
 export const createApp = ViteSSG(App, { routes })
