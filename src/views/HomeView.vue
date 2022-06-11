4ca28b3c376ddc9d4b63434606754ca9a724c97d
Xiangdong Zeng <xdzeng96@gmail.com>
2022-06-12 04:10:34 +0800

    basic structure for home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,41 @@
 <script setup lang="ts">
-import { typefaces } from '@/data'
+import { computed, onMounted, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { dash, works } from '@/data'
+import { flat, shuffle } from '@/utils'
+import LinkFigure from '@/components/LinkFigure.vue'
+
+const route = useRoute()
+const router = useRouter()
+
+// const items = computed(() => shuffle(flat([works, dash].map(Object.entries))))
+const items = computed(() => shuffle(Object.entries(works)))
+
+const to = (id: string) => `${router.resolve(id in works ? 'works' : 'dash').path}/${id}`
+
+const itemIndex = ref<number>(0)
+const updateItemIndex = () => (itemIndex.value = (itemIndex.value + 1) % items.value.length)
+onMounted(() => setInterval(updateItemIndex, 600))
 </script>
 
 <template>
-  <main>
-    <!--
-    <div v-for="(item, name) in typefaces">
-      <h3 class="my-8 text-lg">{{ name }}</h3>
-      <ul class="list-disc pl-4">
-        <li v-for="i in item">{{ i }}</li>
-      </ul>
-    </div>
-    -->
-  </main>
+  <div>
+    <Transition v-for="(entry, i) in items">
+      <div v-if="itemIndex === i" class="fixed flex h-full items-center pr-4 lg:pr-8">
+        <LinkFigure :image="entry[1].images[0]" :to="to(entry[0])" />
+      </div>
+    </Transition>
+  </div>
 </template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  @apply duration-300;
+}
+
+.v-enter-from,
+.v-leave-to {
+  @apply opacity-0;
+}
+</style>
