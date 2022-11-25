7f28cdcf1357edf0c4d5b8e5eefc5036e2882e2d
Xiangdong Zeng <xdzeng96@gmail.com>
2022-11-25 21:44:08 +0800

    update dash subscription

--- src/components/dash/DashGet.vue
+++ src/components/dash/DashGet.vue
@@ -1,33 +1,23 @@
 <script setup lang="ts">
-defineProps<{
-  data: {
-    printed: {
-      summary: string
-      stores: {
-        name: string
-        url?: string
-      }[]
-    }
-    digital: {
-      summary: string
-      emailLabel: string
-      submitLabel: string
-      notice: string
-    }
-  }
-}>()
+import stores from '@/components/dash/stores.json'
+import DashSubscription from '@/components/dash/DashSubscription.vue'
+
+const storeName = (s: { name: string; city?: string }) =>
+  s.city ? `${s.name}（${s.city}）` : s.name
 </script>
 
 <template>
-  <div>
-    <h3 class="mb-4 sm:mb-8">{{ data.printed.summary }}</h3>
-    <ul class="mb-4 list-disc pl-4 sm:mb-8 lg:columns-2 lg:gap-16">
-      <li v-for="store in data.printed.stores">
-        <a v-if="store.url" :href="store.url" class="after:content-['_↗']">{{ store.name }}</a>
-        <template v-else>{{ store.name }}</template>
+  <div class="flex flex-col gap-4 sm:gap-8">
+    <h3>纸质版购买 / Printed version available in:</h3>
+    <ul class="list-disc pl-4 lg:columns-2 lg:gap-16">
+      <li v-for="store in stores">
+        <a v-if="store.url" :href="store.url" class="after:content-['↗']">
+          {{ storeName(store) }}
+        </a>
+        <template v-else>{{ storeName(store) }}</template>
       </li>
     </ul>
-    <h3 class="mb-4 sm:mb-8">{{ data.digital.summary }}</h3>
-    <p><!-- TODO --></p>
+    <h3>电子版订阅 / Digital version subscription:</h3>
+    <DashSubscription />
   </div>
 </template>
---
+++ src/components/dash/InputEmail.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+defineProps<{
+  id: string
+  label: string
+  name: string
+  placeholder?: string
+  invalidFeedback?: string
+}>()
+</script>
+
+<template>
+  <div>
+    <label :for="id" class="hidden">{{ label }}</label>
+    <input
+      type="email"
+      required
+      :id="id"
+      :name="name"
+      :placeholder="placeholder"
+      class="w-full border-b-2 border-primary bg-transparent"
+    />
+    <p
+      v-if="invalidFeedback"
+      class="InvalidFeedback max-h-0 overflow-hidden text-xs text-red-500 duration-300"
+    >
+      {{ invalidFeedback }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+input[type='email']::placeholder {
+  @apply text-neutral-900 !text-opacity-50 dark:text-neutral-100;
+}
+
+input[type='email']:invalid:not(:focus):not(:placeholder-shown) ~ .InvalidFeedback {
+  @apply mt-1 max-h-8;
+}
+</style>
