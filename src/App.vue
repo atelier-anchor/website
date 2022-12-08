c3d3db268281638c50c26268a6a8df05f924120f
Xiangdong Zeng <xdzeng96@gmail.com>
2022-12-09 03:55:24 +0800

    optimize seo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { useHead } from '@vueuse/head'
 import SiteHeader from '@/components/header/SiteHeader.vue'
 import SiteFooter from '@/components/SiteFooter.vue'
+
+const title = 'atelierAnchor'
+const description = 'graphic & typeface design studio in shanghai. we make typeface as A narrative.'
+const url = 'https://atelier-anchor.com'
+const date = new Date(import.meta.env.VITE_BUILD_DATE)
+
+useHead({
+  meta: [
+    { name: 'description', content: description },
+    { name: 'copyright', content: `© ${date.getFullYear()} ${title}` },
+    { name: 'build-time', content: date.toISOString() },
+    { name: 'twitter:card', content: 'summary' },
+    { name: 'twitter:site', content: '@atelier_anchor' },
+    { name: 'twitter:title', content: title },
+    { name: 'twitter:description', content: description },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: url },
+    { property: 'og:title', content: title },
+    { property: 'og:description', content: description },
+  ],
+})
 </script>
 
 <template>
