3c787ddc066133ec0cae7f8f49677b4fd9eb2eec
Xiangdong Zeng <xdzeng96@gmail.com>
2022-12-08 22:34:38 +0800

    add width and height for logo

--- src/components/header/SiteLogo.vue
+++ src/components/header/SiteLogo.vue
@@ -7,7 +7,7 @@ import { RouterLink } from 'vue-router'
     <RouterLink to="/" class="inline-block w-32 sm:w-40">
       <picture>
         <source srcset="/atelier-anchor.dark.svg" media="(prefers-color-scheme: dark)" />
-        <img src="/atelier-anchor.light.svg" alt="atelierAnchor" />
+        <img src="/atelier-anchor.light.svg" alt="atelierAnchor" width="333.42" height="125.06" />
       </picture>
     </RouterLink>
   </h1>
