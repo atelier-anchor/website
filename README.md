6d6543b427eb683e10f3c4330bafe01f99438892
Xiangdong Zeng <xdzeng96@gmail.com>
2025-03-07 00:29:57 +0800

    chore: update address and license

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # atelier-anchor.com
 
 ![GitHub deployments](https://img.shields.io/github/deployments/atelier-anchor/website/production?label=vercel&logo=vercel)
+
+## License
+
+Code in this project is licensed under the [MIT License](./LICENSE).
--- app/components/site/SiteFooterAddress.vue
+++ app/components/site/SiteFooterAddress.vue
@@ -1,8 +1,6 @@
 <template>
   <address class="not-italic">
-    101 A, No. 15, Lane 545<br />
-    Zhenning Road<br />
-    Changning District<br />
+    Songjiang District<br />
     Shanghai, China<br />
     <NuxtLink to="mailto:hi@atelier-anchor.com">hi@atelier-anchor.com</NuxtLink>
   </address>
